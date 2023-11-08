41565688750a350db3c7158edbe03ef1459889f6
Thanushan Satheeskumar <80972937+Thanushan925@users.noreply.github.com>
2023-11-07 20:36:21 -0500

    Edited Browsing Page to include currently airing Movies using Cineplex API

--- final_app/lib/ui/browsing_page.dart
+++ final_app/lib/ui/browsing_page.dart
@@ -1,98 +1,104 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Movie {
+  final int id;
+  final String name;
+  final String runtime;
+  final String releaseDate;
+  final String smallPosterImageUrl;
+
+  Movie({
+    required this.id,
+    required this.name,
+    required this.runtime,
+    required this.releaseDate,
+    required this.smallPosterImageUrl,
+  });
+}
+
+Future<List<Movie>> fetchMovies() async {
+  final response = await http.get(Uri.parse('https://www.cineplex.com/api/v1/movies?language=en-us&marketLanguageCodeFq'));
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    final List<dynamic> moviesData = jsonResponse['data'];
+    final List<Movie> movies = moviesData.map((movie) {
+      return Movie(
+        id: movie['id'] as int,
+        name: movie['name'] as String,
+        runtime: movie['duration'] as String,
+        releaseDate: movie['releaseDate'] as String,
+        smallPosterImageUrl: movie['smallPosterImageUrl'] as String,
+      );
+    }).toList();
+
+    return movies;
+  } else {
+    throw Exception('Failed to load movies');
+  }
+}
+
+void main() => runApp(const BrowsingPage());
 
 class BrowsingPage extends StatefulWidget {
+  const BrowsingPage({Key? key});
+
   @override
   _BrowsingPageState createState() => _BrowsingPageState();
 }
 
 class _BrowsingPageState extends State<BrowsingPage> {
-  bool _showSearchBar = false;
-  bool _sortAscending = true; // Added for sorting order
+  late Future<List<Movie>> futureMovies;
 
-  // Sample movie names
-  List<String> movieNames = [
-    'Movie 1',
-    'Movie 2',
-    'Movie 3',
-  ];
+  @override
+  void initState() {
+    super.initState();
+    futureMovies = fetchMovies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Sort the movie names based on the current sorting order
-    movieNames.sort((a, b) {
-      if (_sortAscending) {
-        return a.compareTo(b);
-      } else {
-        return b.compareTo(a);
-      }
-    });
-    return Scaffold(
-      appBar: AppBar(
-        title: _showSearchBar
-            ? TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search Movies',
-                  border: InputBorder.none,
-                ),
-                style: TextStyle(color: Colors.white),
-                autofocus: true,
-                onChanged: (query) {
-                  // Handle search logic as the user types
-                },
-              )
-            : Text('Movies'),
-        actions: [
-          IconButton(
-            icon: Icon(_showSearchBar ? Icons.close : Icons.search),
-            onPressed: () {
-              setState(() {
-                _showSearchBar = !_showSearchBar;
-              });
+    return MaterialApp(
+      title: 'Movie List',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Movies'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Movie>>(
+            future: futureMovies,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final movies = snapshot.data!;
+
+                return ListView.builder(
+                  itemCount: movies.length,
+                  itemBuilder: (context, index) {
+                    final movie = movies[index];
+
+                    return ListTile(
+                      leading: Image.network(movie.smallPosterImageUrl),
+                      title: Text(movie.name),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Runtime: ${movie.runtime}'),
+                          Text('Release Date: ${movie.releaseDate}'),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              return const CircularProgressIndicator();
             },
           ),
-          IconButton(
-            icon: Icon(Icons.sort),
-            onPressed: () {
-              setState(() {
-                _sortAscending = !_sortAscending;
-              });
-            },
-          ),
-        ],
-      ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Popular Movies',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          for (String movieName in movieNames)
-            ListTile(
-              title: Text(movieName),
-            ),
-          // Add widgets for popular movies here
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Now Playing',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          // Add widgets for now playing movies here
-          for (String movieName in movieNames)
-            ListTile(
-              title: Text(movieName),
-            ),
-        ],
+        ),
       ),
     );
   }
